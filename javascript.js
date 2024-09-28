3285a16eddceb96da5df7da23c47f0f4bed83c3f
HezerSantos <HezerSantos24@gmail.com>
2024-09-28 00:44:43 -0500

    Completed UI with js implementation

--- javascript.js
+++ javascript.js
@@ -10,13 +10,6 @@ function getComputerChoice(){
 }
 
 
-function getHumanChoice(){
-    choice = prompt('Enter either Rock, Paper, or Scissors: ')
-    return choice
-}
-
-
-
 function playRound(humanChoice, computerChoice){
     if (humanChoice.toLowerCase() === computerChoice.toLowerCase()){
         return "There was a draw";
@@ -30,24 +23,93 @@ function playRound(humanChoice, computerChoice){
         humanScore++;
         return 'You win! Scissors Beats Paper';
     } else {
-        computerScore ++
-        return 'You lose! Scissors Beats Paper';
+        computerScore ++;
+        return `You lose! ${computerChoice} Beats ${humanChoice}`;
     }
 }
 
 let humanScore = 0
 let computerScore = 0
 
-function playGame(){
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    return playRound(humanSelection, computerSelection);
-}
 
-for (i = 1; i <= 5; i++){
-    console.log(playGame())
-}
+//block for buttons
+const rockButton = document.querySelector('#rockBtn')
+const paperButton = document.querySelector('#paperBtn')
+const scissorsButton = document.querySelector('#scissorsBtn')
 
-console.log(`Results:`)
-console.log(`Your Score: ${humanScore}`)
-console.log(`Computer Score: ${computerScore}`) //u stupid lolss
+//results box
+const resultsBox = document.querySelector('#resultsBox')
+
+//block for scores
+const humanHTML = document.createElement('p')
+const computerHtml = document.createElement('p')
+humanHTML.textContent = `Your score: ${humanScore}`
+computerHtml.textContent = `Computer score: ${computerScore}`
+resultsBox.appendChild(humanHTML)
+resultsBox.appendChild(computerHtml)
+
+//P tags
+const PTag1 = document.createElement('p')
+const ptag2 = document.createElement('p')
+
+//Winner
+ptag2.textContent = 'The winner is: '
+resultsBox.appendChild(ptag2)
+
+
+
+//Events
+rockButton.addEventListener("click", () => {
+    let result = playRound('rock', getComputerChoice())
+    PTag1.textContent = `${result}`
+    resultsBox.appendChild(PTag1)
+    humanHTML.textContent = `Your score: ${humanScore}`
+    computerHtml.textContent = `Computer score: ${computerScore}`
+    
+})
+
+paperButton.addEventListener("click", () => {
+    let result = playRound('paper', getComputerChoice())
+    PTag1.textContent = `${result}`
+    resultsBox.appendChild(PTag1)
+    humanHTML.textContent = `Your score: ${humanScore}`
+    computerHtml.textContent = `Computer score: ${computerScore}`
+    
+})
+
+scissorsButton.addEventListener("click", () => {
+    let result = playRound('scissors', getComputerChoice())
+    PTag1.textContent = `${result}`
+    resultsBox.appendChild(PTag1)
+    humanHTML.textContent = `Your score: ${humanScore}`
+    computerHtml.textContent = `Computer score: ${computerScore}`
+    
+})
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (humanScore === 5){
+            ptag2.textContent = 'The winner is: HUMAN'
+            resultsBox.appendChild(ptag2)
+        }
+        else if (computerScore === 5){
+            ptag2.textContent = 'The winner is: Computer'
+            resultsBox.appendChild(ptag2)
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
